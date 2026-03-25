bf37484307b828be89ec30e55e142daa1df69355
sadini-2001 <sadini.weerappulige@gmail.com>
2026-03-25 12:18:24 +0530

    Update 03_etl_procedure.sql
    Updated ETL procedure to log invalid data and handle future dates.

--- sql/03_etl_procedure.sql
+++ sql/03_etl_procedure.sql
@@ -10,7 +10,8 @@ BEGIN
     DELETE FROM master_policies;
     TRUNCATE TABLE data_error_logs;
 
-    INSERT INTO master_policies (
+    -- STEP 1: Insert CLEAN data
+    INSERT IGNORE INTO master_policies (   
         customer_name,
         policy_category,
         premium_decimal,
@@ -40,8 +41,10 @@ BEGIN
         AND LENGTH(premium_amt) <= 7
         AND CAST(premium_amt AS DECIMAL(10,2)) > 0
         AND policy_start_date IS NOT NULL
-        AND policy_start_date REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
+        AND policy_start_date REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
+        AND STR_TO_DATE(policy_start_date, '%Y-%m-%d') <= CURDATE();
 
+    -- STEP 2: Log INVALID data
     INSERT INTO data_error_logs (source_id, issue_description)
     SELECT 
         raw_id,
@@ -56,6 +59,8 @@ BEGIN
             WHEN CAST(premium_amt AS DECIMAL(10,2)) <= 0 THEN 'Negative or Zero Premium'
             WHEN policy_start_date IS NULL THEN 'Missing Date'
             WHEN policy_start_date NOT REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$' THEN 'Invalid Date Format'
+            WHEN STR_TO_DATE(policy_start_date, '%Y-%m-%d') > CURDATE() THEN 'Future Date'
+
             ELSE 'Unknown Issue'
         END
 
@@ -70,7 +75,9 @@ BEGIN
         OR LENGTH(premium_amt) > 7
         OR CAST(premium_amt AS DECIMAL(10,2)) <= 0
         OR policy_start_date IS NULL
-        OR policy_start_date NOT REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
+        OR policy_start_date NOT REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
+        
+        OR STR_TO_DATE(policy_start_date, '%Y-%m-%d') > CURDATE();
 
 END //
 
